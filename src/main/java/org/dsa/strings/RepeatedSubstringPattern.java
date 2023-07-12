82983f587ce4f8b8b86be6f697f1f43bd874bdae
Shama Ugale <Shama.Ugale@gmail.com>
2023-07-13 00:13:17 +0530

    added sliding window problem 1

---
+++ src/main/java/org/dsa/strings/RepeatedSubstringPattern.java
@@ -0,0 +1,53 @@
+package org.dsa.strings;
+
+/*
+Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
+
+
+
+Example 1:
+
+Input: s = "abab"
+Output: true
+Explanation: It is the substring "ab" twice.
+Example 2:
+
+Input: s = "aba"
+Output: false
+Example 3:
+
+Input: s = "abcabcabcabc"
+Output: true
+Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
+ */
+public class RepeatedSubstringPattern {
+
+    public static boolean repeatedSubstringPattern(String s) {
+        // corner case
+        if(s == null || s.length() == 0) return true;
+
+        int n = s.length();
+        for(int len = 1; len <= n/2; len++){ // pattern must repeat at least twice, i.e. pattern length is at most n/2
+            if(n % len != 0) continue; // s length must can be divided by the pattern length
+
+            String pattern = s.substring(0, len); // pattern string
+            int i = len; // start index of 2nd pattern
+            int j = i + len - 1; // end index of 2nd pattern
+            while(j < n){
+                String substr = s.substring(i, j + 1);
+                if(!pattern.equals(substr)) break; // failed for this pattern, try next pattern
+                i += len;
+                j += len;
+            }
+
+            // if it past the last substring check, i will be n
+            if(i == n) return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(repeatedSubstringPattern("abab"));
+
+    }
+}
